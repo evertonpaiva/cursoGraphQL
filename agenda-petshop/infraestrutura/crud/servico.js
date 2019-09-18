00b688f1cb9805db0f534c162d7866a055efe67f
Everton Paiva <evertonpaiva@gmail.com>
2019-09-18 09:12:54 -0300

    Criando um serviço

--- agenda-petshop/infraestrutura/crud/servico.js
+++ agenda-petshop/infraestrutura/crud/servico.js
@@ -4,33 +4,38 @@ class Servico {
   lista(res) {
     const sql = 'SELECT * FROM Servicos';
 
-    executaQuery(res, sql)
+    executaQuery(res, sql);
   }
 
   buscaPorId(res, id) {
     const sql = `SELECT * FROM Servicos WHERE id=${parseInt(id)}`;
 
-    executaQuery(res, sql)
+    executaQuery(res, sql);
   }
 
-  adiciona(res, item) {
+  adiciona(item) {
     const { nome, preco, descricao } = item;
     const sql = `INSERT INTO Servicos(nome, Preco, Descricao) VALUES('${nome}', ${preco}, '${descricao}')`;
 
-    executaQuery(res, sql)
+    return executaQuery(sql).then(resposta => ({
+      id: resposta.insertId,
+      nome,
+      preco,
+      descricao
+    }));
   }
 
   atualiza(res, novoItem, id) {
     const { nome, preco, descricao } = novoItem;
     const sql = `UPDATE Servicos SET nome='${nome}', Preco=${preco}, Descricao='${descricao}' WHERE id=${id}`;
 
-    executaQuery(res, sql)
+    executaQuery(res, sql);
   }
 
   deleta(res, id) {
     const sql = `DELETE FROM Servicos WHERE id=${id}`;
 
-    executaQuery(res, sql)
+    executaQuery(res, sql);
   }
 }
 
