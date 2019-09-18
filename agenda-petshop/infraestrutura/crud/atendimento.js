fdec57fdb91558bf5df2e1a7bf5b0928c0d96f24
Everton Paiva <evertonpaiva@gmail.com>
2019-09-18 12:58:11 -0300

    Criando um atendimento

--- agenda-petshop/infraestrutura/crud/atendimento.js
+++ agenda-petshop/infraestrutura/crud/atendimento.js
@@ -2,24 +2,35 @@ const executaQuery = require('../database/queries');
 
 class Atendimento {
   lista(res) {
-    const sql = 'SELECT * FROM Atendimentos';
+    const sql = `SELECT * FROM Atendimentos`;
 
-    executaQuery(res, sql)
+    executaQuery(res, sql);
   }
 
   buscaPorId(res, id) {
     const sql = `SELECT * FROM Atendimentos WHERE id=${parseInt(id)}`;
 
-    executaQuery(res, sql)
+    executaQuery(res, sql);
   }
 
-  adiciona(res, item) {
-    const { cliente, pet, servico, status, observacoes } = item;
+  adiciona(item) {
+    const { clienteId, petId, servicoId, status, observacoes } = item;
     const data = new Date().toLocaleDateString();
 
-    const sql = `INSERT INTO Atendimentos(clienteId, petId, servicoId, data, status, observacoes) VALUES(${cliente}, ${pet}, ${servico}, '${data}', '${status}', '${observacoes}')`
+    const sql = `
+      INSERT INTO Atendimentos(clienteId, petId, servicoId, data, status, observacoes) 
+      VALUES(${clienteId}, ${petId}, ${servicoId}, '${data}', '${status}', '${observacoes}')
+    `;
 
-    executaQuery(res, sql)
+    return executaQuery(sql).then(dados => ({
+      id: dados.insertId,
+      clienteId,
+      petId,
+      servicoId,
+      data,
+      status,
+      observacoes
+    }));
   }
 
   atualiza(res, novoItem, id) {
@@ -28,13 +39,13 @@ class Atendimento {
 
     const sql = `UPDATE Atendimentos SET clienteId=${cliente}, petId=${pet}, servicoId=${servico}, data='${data}', status='${status}' observacoes='${observacoes}' WHERE id=${id}`;
 
-    executaQuery(res, sql)
+    executaQuery(res, sql);
   }
 
   deleta(res, id) {
     const sql = `DELETE FROM Atendimentos WHERE id=${id}`;
 
-    executaQuery(res, sql)
+    executaQuery(res, sql);
   }
 }
 
